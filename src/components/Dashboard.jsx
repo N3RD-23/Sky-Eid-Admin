d456d00ca5cf630ce5e1818c3c0b8166152d1c23
Nerd <mohamedshunaan10@gmail.com>
2024-06-12 07:38:15 -0700

    Added Dashboard Elements

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,7 +1,58 @@
 import React, { useState, useEffect } from "react";
-import { Card, Col, Row, Statistic } from 'antd';
+import { Card, Col, Row, Statistic, Typography } from "antd";
+import { useSpring, animated } from "react-spring";
+
+const { Title } = Typography;
+
+const titleStyle = {
+  color: '#808080'
+}
+
+const textStyle = {
+  color: '#00A36C'
+}
 
 const Dashboard = () => {
+  const Teams = () => {
+    const { number } = useSpring({
+      from: { number: 0 },
+      number: teams.length,
+      delay: 200,
+      config: { mass: 1, tension: 20, friction: 10 },
+    });
+    return <animated.div>{number.to((n) => n.toFixed(0))}</animated.div>;
+  };
+
+  const Players = () => {
+    const { number } = useSpring({
+      from: { number: 0 },
+      number: teams.length*4,
+      delay: 200,
+      config: { mass: 1, tension: 20, friction: 10 },
+    });
+    return <animated.div>{number.to((n) => n.toFixed(0))}</animated.div>;
+  };
+
+  const ExpectedNumber = () => {
+    const { number } = useSpring({
+      from: { number: 0 },
+      number: 15,
+      delay: 200,
+      config: { mass: 1, tension: 20, friction: 10 },
+    });
+    return <animated.div>{number.to((n) => n.toFixed(0))}</animated.div>;
+  };
+
+  const ExpectedPlayers = () => {
+    const { number } = useSpring({
+      from: { number: 0 },
+      number: 40,
+      delay: 200,
+      config: { mass: 1, tension: 20, friction: 10 },
+    });
+    return <animated.div>{number.to((n) => n.toFixed(0))}</animated.div>;
+  };
+
   const [teams, setTeams] = useState([]);
   useEffect(() => {
     fetch("https://api.sky.ngo/players")
@@ -15,46 +66,35 @@ const Dashboard = () => {
   }, []);
 
   return (
-    <div className="Teams" style={{ height: '79vh' }}>
+    <div className="Teams" style={{ height: "79vh" }}>
       {/* <h1>Dashboard</h1> */}
       <Row gutter={16}>
-    <Col span={12} style={{ marginBottom: '10px' }}>
-      <Card bordered={false}>
-        <Statistic
-          title="Total Teams"
-          value={teams.length}
-          valueStyle={{ color: '#3f8600' }}
-        />
-      </Card>
-    </Col>
-    <Col span={12}>
-      <Card bordered={false}>
-        <Statistic
-          title="Expected Number of Teams"
-          value={10}
-          valueStyle={{ color: '#cf1322' }}
-        />
-      </Card>
-    </Col>
-    <Col span={12}>
-      <Card bordered={false}>
-        <Statistic
-          title="Total Players"
-          value={teams.length * 4}
-          valueStyle={{ color: '#3f8600' }}
-        />
-      </Card>
-    </Col>
-    <Col span={12}>
-      <Card bordered={false}>
-        <Statistic
-          title="Expected Number of Players"
-          value={40}
-          valueStyle={{ color: '#cf1322' }}
-        />
-      </Card>
-    </Col>
-  </Row>
+        <Col span={12} style={{ marginBottom: "10px" }}>
+          <Card bordered={false}>
+            <Title level={3} style={titleStyle}>Total Teams</Title>
+            <Title level={3} style={textStyle}><Teams /></Title>
+          </Card>
+        </Col>
+        <Col span={12} style={{ marginBottom: "10px" }}>
+          <Card bordered={false}>
+            <Title level={3} style={titleStyle}>Expected Number of Teams</Title>
+            <Title level={3} style={textStyle}><ExpectedNumber /></Title>
+          </Card>
+        </Col>
+        <Col span={12} style={{ marginBottom: "10px" }}>
+          <Card bordered={false}>
+            <Title level={3} style={titleStyle}>Total Players</Title>
+            <Title level={3} style={textStyle}><Players /></Title>
+          </Card>
+        </Col>
+        <Col span={12} style={{ marginBottom: "10px" }}>
+          <Card bordered={false}>
+            <Title level={3} style={titleStyle}>Expected Number of Players</Title>
+            <Title level={3} style={textStyle}><ExpectedPlayers /></Title>
+          </Card>
+        </Col>
+        
+      </Row>
     </div>
   );
 };
